4691cfe5b219ba66a05d9858999bedd65ee58d10
mahmud.asrul <mahmud.asrul@ui.ac.id>
2022-02-10 16:25:23 +0800

    menambahkan file

---
+++ KalkulatorDetik.java
@@ -0,0 +1,28 @@
+public class KalkulatorDetik{
+    public static void main(String[] args) {
+    //input jam berapa
+    //harus 0 =< jam =< 24, else : mohon maaf gabisa
+    //input menit berapa
+    //harus 0 =< menit =< 60, else : mohon maaf gabisa
+    //input mau detik berapa
+    //harus 0 =< detik =< 60, else : mohon maaf gabisa harus sesuai persyaratan
+    
+    //mau berapa N (detik setelahnya?)
+    //jika 
+    //detik = detik + N % (berapa detik dalam satu menit)
+
+        //yang jadi pertanyaan gimana cara dapet dividennya ?
+    //menit = 
+    //
+    //
+
+
+    byte jam, menit;
+    input jam =
+    input menit = 
+
+    input =
+    
+    }
+}
+  
